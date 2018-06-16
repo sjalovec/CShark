427e84643c61d9324e69ff5b5dfb02d772561fb2
Štěpán Jalovec <s.jalovec@seznam.cz>
2018-06-16 20:25:50 +0200

    201806162025

---
+++ ITNetwork/ITNetwork/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj, jsem virutální papoušek Lóra, rád opakuji!");
+            Console.WriteLine("Napiš něco: ");
+
+            string vstup;
+            vstup = Console.ReadLine();
+
+            string vystup;
+            vystup = vstup + " " + vstup + "!";
+
+            Console.WriteLine(vystup);
+            Console.ReadKey();
+        }
+    }
+}
