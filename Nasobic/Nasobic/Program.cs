abde312fd5a70e3525d704a9268883341f678c04
Štěpán Jalovec <s.jalovec@seznam.cz>
2018-06-16 21:05:52 +0200

    Čísla + parsování

---
+++ Nasobic/Nasobic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasobic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte číslo k zdvojnásobení");
+            string s = Console.ReadLine();
+            int a = int.Parse(s);
+            a = a * 2;
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
